73628e2fce8fe60c617c79222df5e436e4255da5
Toshine <toshineluv99@yahoo.com>
2020-04-20 22:50:22 +0100

    project setup and table migration added

---
+++ database/migrations/2020_04_20_214750_create_movie_tables.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovieTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->double("voteAverage");
+            $table->string("releaseDate");
+            $table->integer("runtime");
+            $table->string("backdropPath");
+            $table->integer("budget");
+            $table->string("posterPath");
+            $table->string("status");
+            $table->string("homepage");
+            $table->timestamps();
+        });
+
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string("site");
+            $table->string("name");
+            $table->string("video_id_num");
+            $table->string("type");
+            $table->string("key");
+            $table->timestamps();
+        });
+
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->integer("genre_id_num");
+            $table->timestamps();
+        });
+
+        Schema::create('genre_movie', function (Blueprint $table) {
+            $table->id();
+            $table->integer("genre_id");
+            $table->integer("movie_id");
+            $table->timestamps();
+        });
+
+        Schema::create('movie_video', function (Blueprint $table) {
+            $table->id();
+            $table->integer("movie_id");
+            $table->integer("video_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(['movies', 'videos', 'genres', 'genre_movie', 'movie_video']) ;
+    }
+}
